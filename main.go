81b56629f43c32e0a50b43b02eddd11a57016fe1
Leslie_Jiang <Leslie_Jiang@proton.me>
2024-06-14 18:36:28 +0800

    feat: noroute

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,11 +12,23 @@ func main() {
 	r.Use(gin.Logger(), gin.Recovery())
 
 
-	r.GET("/", func(c *gin.Context) {
+	r.GET("/foo", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"Hello": "World",
 		})
 	})
 
+	r.NoRoute(func(c *gin.Context) {
+		accept := c.Request.Header.Get("Accept")
+		if strings.Contains(accept, "text/html") {
+			c.String(http.StatusNotFound, "Not Found")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code": 404,
+				"error_msg": "Not Found",
+			})
+		}
+	})
+	
 	r.Run(":8000")
 }
