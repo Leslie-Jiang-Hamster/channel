e171dcf49f38ad3771a0fb9815b2edee4d5f993a
Leslie_Jiang <Leslie_Jiang@proton.me>
2024-06-15 13:18:06 +0800

    feat: router group

--- routes/api.go
+++ routes/api.go
@@ -7,6 +7,10 @@ import (
 )
 
 func SetupAPIRoutes(router *gin.Engine) {
+	setUpV1Routes(router)
+}
+
+func setUpV1Routes(router *gin.Engine) {
 	v1 := router.Group("/v1")
 	v1.GET("/foo", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
